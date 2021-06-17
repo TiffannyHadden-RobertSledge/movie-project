fc5e2a097005fd843830e95e54a4f81bf423c03e
Sledgerobby35 <sledge.robby35@gmail.com>
2021-06-17 14:27:16 -0500

    allow users to add new movies

--- js/fetch.js
+++ js/fetch.js
@@ -1,29 +1,48 @@
 "use strict";
 getAllMovies()
-displayMovies(buildMovieList(getAllMovies))
+
+// displayMovies(buildMovieList(getAllMovies))
 
 
 function getAllMovies() {
-    fetch("https://hazel-distinct-waiter.glitch.me/movies", {
-        method: "GET",
-    })
-        .then(response => response.json())
-        .then(data => console.log(data))
+	fetch("https://hazel-distinct-waiter.glitch.me/movies", {
+		method: "GET",
+	})
+		.then(response => response.json())
+		.then(data => {
+			displayMovies((buildMovieList(data)))
+		})
 }
 
 window.onload = function () {
-    alert("Window is loading");
+	alert("Window is loading");
 }
 
 function buildMovieList(data) {
-    for (let i = 0; i < data.length; i++) {
-        let output = "";
-        output += "<h4>" + data[i].title +"</h4>";
-        output += "<p>" + data[i].actors +"</p>";
-        output += "<p>" + data[i].plot +"</p>";
-        output += "<p>" + data[i].rating +"</p>";
-        return output;
-    }}
-    function displayMovies(listOfMovies) {
-    $("#movies").html(listOfMovies)
-    }
+	let output = "";
+	for (let i = 0; i < data.length; i++) {
+		output += "<h4>" + data[i].title + "</h4>";
+		output += "<p>" + data[i].actors + "</p>";
+		output += "<p>" + data[i].plot + "</p>";
+		output += "<p>" + data[i].rating + "</p>";
+	}
+	return output;
+}
+
+function displayMovies(listOfMovies) {
+	$("#movies").append(listOfMovies);
+}
+
+$("#submitButton").on("click", function (event) {
+	event.preventDefault();
+	let postTitle = $("#title").value
+	let postRating = $("#rating").value
+	console.log(postTitle);
+	console.log(postRating);
+	fetch("https://hazel-distinct-waiter.glitch.me/movies", {
+		method: "POST",
+		headers: {},
+		body: {title: postTitle, rating: postRating}
+	})
+		.then(response => console.log(response.json()));
+})
