88de9a7008786af86e50e6cd6b15b3465fab141f
Sledgerobby35 <sledge.robby35@gmail.com>
2021-06-18 15:52:53 -0500

    add edit movie functionality to individual movies

--- js/fetch.js
+++ js/fetch.js
@@ -2,7 +2,19 @@
 getAllMovies();
 
 // displayMovies(buildMovieList(getAllMovies))
+window.onload = function () {
+	// alert("Window is loading");
+}
 
+$("#submitButton").on("click", function () {
+	console.log("adding movie");
+	addMovie();
+});
+
+$(".editButton").on("click", function() {
+	console.log("clicked");
+	$(".hideMe").toggle();
+})
 
 function getAllMovies() {
 	fetch("https://hazel-distinct-waiter.glitch.me/movies", {
@@ -15,18 +27,18 @@ function getAllMovies() {
 		})
 }
 
-window.onload = function () {
-	// alert("Window is loading");
-}
-
 function buildMovieList(data) {
 	let output = "";
 	$("#movies").empty();
 	for (let i = 0; i < data.length; i++) {
+		output += "<div class='card'>";
 		output += "<h4>" + data[i].title + "</h4>";
 		output += "<p>" + data[i].actors + "</p>";
 		output += "<p>" + data[i].plot + "</p>";
 		output += "<p>" + data[i].rating + "</p>";
+		output += "<button type='button' class='editButton'>Edit Movie</button>"
+		output += buildEditForm(data[i]);
+		output += "</div>"
 	}
 	return output;
 }
@@ -35,10 +47,6 @@ function displayMovies(listOfMovies) {
 	$("#movies").append(listOfMovies);
 }
 
-$("#submitButton").on("click", function (event) {
-	console.log("adding movie")
-	addMovie();
-});
 
 function addMovie() {
 	let title = $("#title").val()
@@ -55,16 +63,62 @@ function addMovie() {
 		.then(data => {
 			console.log(data)
 			getAllMovies()
-			});
-
+		});
 }
 
+function buildEditForm(info) {
+	let form = "<form class='hideMe'>";
 
-fetch("https://hazel-distinct-waiter.glitch.me/movies", {
-	method: "PUT",
-	headers: {
-		'Content-Type': 'application/json',
-	},
-	body: JSON.stringify({title, rating}),
-})
-	.then(response => console.log(response.json()))
+	form += "<label for='editTitle'>Title</label><input id='editTitle'" +
+		" name='editTitle' type='text'>";
+	let title = $("#editTitle");
+	title.attr("value", info.title);
+	console.log(title);
+
+	form += "<label for='editRating'>Rating</label><input id='editRating'" +
+		" name='editRating' type='number' max='5'>";
+	let rating = $("#editRating").val(info.rating);
+
+	form += "<label for='editActors'>Actors</label><input id='editActors'" +
+		" name='editActors' type='text'>";
+	let actors = $("#editActors").val(info.actors);
+
+	form += "<label for='editPlot'>Plot</label><input id='editPlot'" +
+		" name='editPLot' type='text'>";
+	let plot = $("#editPlot").val(info.plot);
+
+	form += "<label for='editDirector'>Director</label><input id='editDirector'" +
+		" name='editDirector' type='text'>";
+	let director = $("#editDirector").val(info.director);
+
+	form += "<label for='editYear'>Year</label><input id='editYear'" +
+		" name='editYear' type='year'>";
+	let year = $("#editYear").val(info.year);
+
+	form += "<label for='editGenre'>Genre</label><input id='editGenre'" +
+		" name='editGenre' type='text'>";
+	let genre = $("#editGenre").val(info.genre);
+
+	form += "<button type='button' class='editMovies' id='editMovies'>Submit" +
+		" Changes</button>"
+
+	form += "</form>";
+
+	$("#editMovies").on("click", function() {
+		console.log("Editing current movie");
+		editMovie(title, rating, director, year, plot, genre, actors);
+	})
+
+	return form;
+}
+
+function editMovie(title, rating, director, year, plot, genre, actors) {
+	fetch("https://hazel-distinct-waiter.glitch.me/movies", {
+		method: "PUT",
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({title, rating, director, year, plot, genre, actors}),
+	})
+		.then(response => console.log(response.json()))
+}
