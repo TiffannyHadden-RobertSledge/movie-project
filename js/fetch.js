4996798d98c7265b8902912fc66793a4316b36bf
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-06-23 14:36:24 -0500

    adds started-stylin

--- js/fetch.js
+++ js/fetch.js
@@ -11,74 +11,61 @@ $("#submitButton").on("click", function () {
 	addMovie();
 });
 
-$(".editButton").on("click", function() {
-	console.log("clicked");
-	$(".hideMe").toggle();
-})
-
-// getAllMovies().then((movies) => {
-//
-// }).catch((error) => {
-//
-// });
-
 function getAllMovies() {
-	console.log("getting all movies")
 	fetch("https://hazel-distinct-waiter.glitch.me/movies", {
 		method: "GET",
 	})
 		.then(response => response.json())
 		.then(data => {
-			console.log('Here are all the movies:');
-			data.forEach(({title, genre, rating, id}) => {
-				let newTitle = `${title}`;
-				let newGenre = `${genre}`;
-				let newRating = `${rating}`;
-				let newID = `${id}`;
-				makeCard(newTitle, newGenre, newRating, newID);
-			});
+			// console.log(data);
+			displayMovies((makeCard(data)))
 		})
 }
 
-function makeCard(title, genre, rating, id) {
+function makeCard(data) {
+	let card = ``;
+	$("#movies").empty();
+	for (let i = 0; i < data.length; i++) {
+		card += `
+		<div class="card">
+		<h4>${data[i].title}</h4>
+		<p>${data[i].actors}</p>
+		<p>${data[i].plot}</p>
+		<p>${data[i].genre}</p>
+		<p>${data[i].rating}</p>
+		<button type="button" class="editButton">Edit Movie</button>
+		<button class="deleteMovie" id="${data[i].id}">Delete Movie</button>
+		${buildEditForm(data[i].id)}
+`
 
-	let card;
-	card = "";
-	card += `<div class="card">`;
-	card += `<div class="card-body">`;
-	card += `<p class="card-text mb-0">${title} <br> ${genre}<br> ${rating}</p>`;
-	card += `</div>`;
-	card += `<div id="">`;
-	card += `<button>Edit Movie</button>`;
-	card += `<button class="deleteMovie" id="${id}">Delete Movie</button>`;
-	card += buildEditForm(title, genre, rating, id);
-	card += `</div>`;
-	card += `</div></div>`;
-	document.getElementById("movies").innerHTML += card;
+		$(".deleteMovie").on("click", function(){
+			let uniqueID = $(this).attr("id");
+			console.log("clicked");
+			deleteMovie(uniqueID);
+		})
 
-	$(".deleteMovie").click(function() {
-		let uniqueID = $(this).attr("id");
-		deleteMovie(uniqueID)
-	})
+	}
+	return card;
 }
+
 
 function displayMovies(listOfMovies) {
 	$("#movies").append(listOfMovies);
+	setToggleEventListener();
+	setEditClickEvent();
 }
 
 
 function addMovie() {
 	let title = $("#title").val()
 	let rating = $("#rating").val()
-	let actors = $("#actors").val()
-	let plot = $("#plot").val()
 
 	fetch("https://hazel-distinct-waiter.glitch.me/movies", {
 		method: "POST",
 		headers: {
 			'Content-Type': 'application/json',
 		},
-		body: JSON.stringify({title, rating, actors, plot}),
+		body: JSON.stringify({title, rating}),
 	})
 		.then(response => response.json())
 		.then(data => {
@@ -87,61 +74,81 @@ function addMovie() {
 		});
 }
 
-function buildEditForm(info) {
+function buildEditForm(id) {
 	let form = "<form class='hideMe'>";
 
-	form += "<label for='editTitle'>Title</label><input id='editTitle'" +
+	form += "<label for='editTitle'>Title</label><input class='editTitle'" +
 		" name='editTitle' type='text'>";
-	let title = $("#editTitle");
-	title.attr("value", info.title);
-	console.log(title);
 
-	form += "<label for='editRating'>Rating</label><input id='editRating'" +
+	form += "<label for='editRating'>Rating</label><input class='editRating'" +
 		" name='editRating' type='number' max='5'>";
-	let rating = $("#editRating").val(info.rating);
 
-	form += "<label for='editActors'>Actors</label><input id='editActors'" +
+
+	form += "<label for='editActors'>Actors</label><input class='editActors'" +
 		" name='editActors' type='text'>";
-	let actors = $("#editActors").val(info.actors);
 
-	form += "<label for='editPlot'>Plot</label><input id='editPlot'" +
+
+	form += "<label for='editPlot'>Plot</label><input class='editPlot'" +
 		" name='editPLot' type='text'>";
-	let plot = $("#editPlot").val(info.plot);
 
-	form += "<label for='editDirector'>Director</label><input id='editDirector'" +
+
+	form += "<label for='editDirector'>Director</label><input class='editDirector'" +
 		" name='editDirector' type='text'>";
-	let director = $("#editDirector").val(info.director);
 
-	form += "<label for='editYear'>Year</label><input id='editYear'" +
+
+	form += "<label for='editYear'>Year</label><input class='editYear'" +
 		" name='editYear' type='year'>";
-	let year = $("#editYear").val(info.year);
 
-	form += "<label for='editGenre'>Genre</label><input id='editGenre'" +
+
+	form += "<label for='editGenre'>Genre</label><input class='editGenre'" +
 		" name='editGenre' type='text'>";
-	let genre = $("#editGenre").val(info.genre);
 
-	form += "<button type='button' class='editMovies' id='editMovies'>Submit" +
+
+	form += `<button type='button' data-value='${id}' class='editMovies'>Submit` +
 		" Changes</button>"
 
 	form += "</form>";
 
-	$("#editMovies").on("click", function() {
-		console.log("Editing current movie");
-		editMovie(title, rating, director, year, plot, genre, actors);
-	})
-
 	return form;
 }
+function setEditClickEvent() {
 
-function editMovie(title, rating, director, year, plot, genre, actors) {
-	fetch("https://hazel-distinct-waiter.glitch.me/movies", {
-		method: "PUT",
+	$(".editMovies").on("click", function () {
+		let editForm = $(this).parent();
+		let movie = {
+			title: editForm.find(".editTitle").val(),
+			plot: editForm.find(".editPlot").val(),
+			actors: editForm.find(".editActors").val(),
+			rating: editForm.find(".editRating").val(),
+			genre: editForm.find(".editGenre").val(),
+			year: editForm.find(".editYear").val()
+		}
+		console.log(movie);
+		editMovie(movie, $(this).attr("data-value"));
+		$("#movies").empty();
+	})
+}
+function editMovie(movie, id) {
+	fetch(`https://hazel-distinct-waiter.glitch.me/movies/${id}`, {
+		method: "PATCH",
 		headers: {
 			'Content-Type': 'application/json',
 		},
-		body: JSON.stringify({title, rating, director, year, plot, genre, actors}),
+		body: JSON.stringify(movie),
 	})
 		.then(response => console.log(response.json()))
+		.then(() => location.reload())
+}
+function setToggleEventListener(){
+	$(".editButton").on("click", function () {
+		$(".hideMe").toggle();
+	})
+
+	$(".deleteMovie").click(function() {
+		let uniqueID = $(this).attr("id");
+		console.log("clicked");
+		deleteMovie(uniqueID)
+	})
 }
 
 function deleteMovie(id) {
@@ -150,9 +157,6 @@ function deleteMovie(id) {
 		headers: {
 			'Content-Type': 'application/json',
 		},
-	}).then(response => console.log("deleting"));
-}
-
-function dummyFunction() {
-
+	}).then(response => console.log("deleting"))
+		.then(() => location.reload())
 }
