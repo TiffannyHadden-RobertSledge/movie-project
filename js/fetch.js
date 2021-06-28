f03afba1cf99d7a7985c5d0274758bc8234c4845
Sledgerobby35 <sledge.robby35@gmail.com>
2021-06-28 16:37:16 -0500

    add styling to page and hover functionality

--- js/fetch.js
+++ js/fetch.js
@@ -27,18 +27,22 @@ function makeCard(data) {
 	$("#movies").empty();
 	for (let i = 0; i < data.length; i++) {
 		card += `
-		<div class="card col-3">
-            <div class="hideOnEdit">
-                <h4>${data[i].title}</h4>
-                <p>${data[i].actors}</p>
-                <p>${data[i].plot}</p>
-                <p>${data[i].genre}</p>
-                <p>${data[i].rating}</p>
-                <button type="button" class="editButton">Edit Movie</button>
-            </div>
-            <button class="deleteMovie" id="${data[i].id}">Delete Movie</button>
-            ${buildEditForm(data[i].id)}
-        </div>
+		<div class="card col-4">
+    <div class="card-img">
+        <img src="${data[i].poster}" alt="${data[i].title}, movie poster" height="100%"
+             width="80%">
+    </div>
+    <div class="hideOnEdit">
+        <h4>${data[i].title}</h4>
+        <p>${data[i].actors}</p>
+        <p>${data[i].plot}</p>
+        <p>${data[i].genre}</p>
+        <p>${data[i].rating}</p>
+        <button type="button" class="editButton">Edit Movie</button>
+    </div>
+    <button class="deleteMovie" id="${data[i].id}">Delete Movie</button>
+    ${buildEditForm(data[i].id)}
+</div>
 `
 		$(".deleteMovie").on("click", function () {
 			let uniqueID = $(this).attr("id");
@@ -75,44 +79,36 @@ function addMovie() {
 			getAllMovies()
 		});
 }
-
 function buildEditForm(id) {
-	let form = "<form class='hideMe'>";
-
-	form += "<label for='editTitle'>Title</label><input class='editTitle'" +
-		" name='editTitle' type='text'>";
-
-	form += "<label for='editRating'>Rating</label><input class='editRating'" +
-		" name='editRating' type='number' max='5'>";
-
-
-	form += "<label for='editActors'>Actors</label><input class='editActors'" +
-		" name='editActors' type='text'>";
-
-
-	form += "<label for='editPlot'>Plot</label><input class='editPlot'" +
-		" name='editPLot' type='text'>";
-
-
-	form += "<label for='editDirector'>Director</label><input class='editDirector'" +
-		" name='editDirector' type='text'>";
-
-
-	form += "<label for='editYear'>Year</label><input class='editYear'" +
-		" name='editYear' type='year'>";
-
-
-	form += "<label for='editGenre'>Genre</label><input class='editGenre'" +
-		" name='editGenre' type='text'>";
-
-
-	form += `<button type="button" data-value="${id}" class="editMovies">Submit` +
-		" Changes</button>"
-
-	form += "</form>";
-
+	let form = `
+	<form class="hideMe">
+	
+	<label for="editTitle">Title</label>
+	<input class="editTitle" name="editTitle" type="text">
+	
+	<label for="editRating">Rating</label>
+	<input class="editRating" name="editRating" type="number" max="5" min="-5">
+	
+	<label for="editActors">Actors/Actresses</label>
+	<input class="editActors" name="editActors" type="text">
+	
+	<label for="editPlot">Plot</label>
+	<input class="editPlot" name="editPlot" type="text">
+	
+	<label for="editDirector">Director</label>
+	<input class="editDirector" name="editDirector" type="text">
+	
+	<label for="editYear">Year</label>
+	<input class="editYear" name="editYear" type="month">
+	
+	<label for="editGenre">Genre</label>
+	<input class="editGenre" name="editGenre" type="text">
+	
+	<button type="button" data-value="${id}" class="editMovies">Submit Changes</button>
+</form>`
 	return form;
 }
+
 
 function setEditClickEvent() {
 
@@ -156,6 +152,17 @@ function setToggleEventListener() {
 		console.log("clicked");
 		deleteMovie(uniqueID)
 	})
+
+	$(".card").hover(unHide, hide);
+}
+function unHide(){
+	$(this).find($(".card-img")).hide();
+	$(this).find($(".hideOnEdit")).show();
+}
+
+function hide() {
+	$(this).find($(".card-img")).show();
+	$(this).find($(".hideOnEdit")).hide();
 }
 
 function deleteMovie(id) {
